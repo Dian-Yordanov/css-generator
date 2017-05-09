b9ca3dc81d6b9899ef4020e2644401f74dfb9b97
Noriend <dian.yordanov.z@gmail.com>
2017-05-09 16:54:19 +0300

    added the initial file config and the json backup

---
+++ CSSgen.py
@@ -0,0 +1,19 @@
+def main():
+
+    # with open('credentials', 'r') as myfile:
+    #     data = myfile.read()
+    #
+    # StringTranslator = []
+    # StringTranslator = data.split('\n')
+    #
+    # user_agent = StringTranslator[0]
+    # client_id = StringTranslator[1]
+    # client_secret = StringTranslator[2]
+    # username = StringTranslator[3]
+    # password = StringTranslator[4]
+
+    print "fgfgf"
+
+
+if __name__ == '__main__':
+    main()
