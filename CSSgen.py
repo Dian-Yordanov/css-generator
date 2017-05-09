d758891b5b5237c5d5f902bb66feb1c0c17b5861
Noriend <dian.yordanov.z@gmail.com>
2017-05-09 21:06:05 +0300

    motherfucking finally managed to get attribute of object in json in python

--- CSSgen.py
+++ CSSgen.py
@@ -1,3 +1,19 @@
+from __future__ import print_function
+import json
+# import simplejson as json
+from collections import namedtuple
+from pprint import pprint
+
+try:
+    from types import SimpleNamespace as Namespace
+except ImportError:
+    # Python 2.x fallback
+    from argparse import Namespace
+
+
+def _json_object_hook(d): return namedtuple('X', d.keys())(*d.values())
+def json2obj(data): return json.loads(data, object_hook=_json_object_hook)
+
 def main():
 
     # with open('credentials', 'r') as myfile:
@@ -12,8 +28,63 @@ def main():
     # username = StringTranslator[3]
     # password = StringTranslator[4]
 
-    print "fgfgf"
-    print "fgfgf"
+    with open('stylish-05-09-2017.json') as data_file:
+        data = data_file.read()
+        # data1 = json.load(data_file)
+    # pprint(data1)
+
+        # Parse JSON into an object with attributes corresponding to dict keys.
+    # x = json.loads(data, object_hook=lambda d: namedtuple('X', d.keys())(*d.values()))
+    # x = json2obj(data)
+
+    # print(data1)
+    # print(data)
+
+    # str1 = str(data)
+    #
+    # print(len(str1))
+    # # str1 = str(dict1)
+    # #
+    # # str1 = ''.join(data)
+    # str1 = str1[1:len(str1)-1]
+
+    # # print(len(str1))
+    # #
+    # print(str1)
+
+    # print(data)
+
+    # data = '{"name": "John Smith", "hometown": {"name": "New York", "id": 123}}'
+
+    data = json2obj(data)
+
+    print(data[0].name)
+
+    # print(vars(data))
+
+    # x = json.loads(data, object_hook=lambda d: Namespace(**d))
+    # print(x)
+
+        # json.loads
+        # # jsonFile = json.dumps(data, separators=(',', ':'), sort_keys=True)
+        # jsonFile = json.dumps(data)
+    # pprint(data)
+    # print jsonFile.dumps("\"code")
+
+    # for 'code' in {data}:
+
+    # for k in {jsonFile}:
+    #     if("code" in k):
+    #         print k
+
+
+
+
+
+    # print jsonFile
+
+    # print "fgfgf"
+    # print "fgfgf"
 
 
 if __name__ == '__main__':
