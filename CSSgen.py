4b0ef578e866311ed0f07fc08145843c12681d15
Noriend <dian.yordanov.z@gmail.com>
2017-05-11 20:30:42 +0300

    cleaned

--- CSSgen.py
+++ CSSgen.py
@@ -13,63 +13,24 @@ from __init__ import format_css
 # def json2obj(data): return json.loads(data, object_hook=_json_object_hook)
 
 def returnCSSFromFileAndName(File,Name):
-    css = ""
-
     x = -1
     x1 = -1
     y = 0
-    y1 = 0
     with open(File) as data_file:
         data = json.load(data_file)
         for dataIndexer in data:
             x = x + 1
-
-            # for string in dataIndexer:
-            #     print(string)
-
-            # print(str(dataIndexer)+"\n")
             if (Name in dataIndexer['name']):
                 y = x
-                # z=""
-                # for string in dataIndexer:
-                #     # print(string + "=" + str(dataIndexer[string]))
-                #     if 'sections' in string:
-                #         for string2 in str(dataIndexer[string]):
-                #             # print string2
-                #             # print string2
-                #             # if 'code' in string2:
-                #             z=z+string2
-                #                 # print string2
-                #         #     # print(string2 + "=" + str(string))
-                #         # print(z)
-        # print data[y]['sections']['code']
         for dataIndexer2 in data[y]['sections']:
             x1 = x1 + 1
-            # print dataIndexer2
             break
-        print data[y]['sections'][x1]['code']
         css = data[y]['sections'][x1]['code']
-
     return css
 
 def main():
 
     data = returnCSSFromFileAndName("stylish-05-09-2017.json", "Xelnect's dark style (inspired)(global)")
-
-        # print dataIndexer2['code']
-            # if ("Xelnect's dark style (inspired)(global)" in dataIndexer2['code']):
-            #     y = x
-
-        # print(data[y]['sections'])
-
-                # print(data[y]['sections']['code'])
-                # print(str(dataIndexer)+"\n")
-                # if 'code' in dataIndexer:
-                #     print(dataIndexer['code'])
-                # elif 'name' in dataIndexer:
-                #     print(dataIndexer['name'])
-
-        # print(data[y]['code'])
 
 
     # css = "/* make input elements more awesome */ input:hover{background-color: red;}"
