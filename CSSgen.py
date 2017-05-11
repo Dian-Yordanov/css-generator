59cd54edf73f54b3f161f0e8f622fac6be7b6f62
Noriend <dian.yordanov.z@gmail.com>
2017-05-11 16:01:43 +0300

    this is just nasty

--- CSSgen.py
+++ CSSgen.py
@@ -1,33 +1,55 @@
-from __future__ import print_function
+# from __future__ import print_function
 import json
-from collections import namedtuple
+# from collections import namedtuple
 
-try:
-    from types import SimpleNamespace as Namespace
-except ImportError:
-    # Python 2.x fallback
-    from argparse import Namespace
+# try:
+#     from types import SimpleNamespace as Namespace
+# except ImportError:
+#     # Python 2.x fallback
+#     from argparse import Namespace
+from __init__ import format_css
 
 
-def _json_object_hook(d): return namedtuple('X', d.keys())(*d.values())
-def json2obj(data): return json.loads(data, object_hook=_json_object_hook)
+# def _json_object_hook(d): return namedtuple('X', d.keys())(*d.values())
+# def json2obj(data): return json.loads(data, object_hook=_json_object_hook)
+
+# def getUnicode(string):
+#     return string.encode('utf-8')
+
 
 def main():
 
     x = -1
     y = 0
-    with open('stylish-05-09-2017wintendo.json') as data_file:
-        # data = data_file.read()
+    with open('stylish-05-09-2017.json') as data_file:
         data = json.load(data_file)
-        # data['id'] = 134  # <--- add `id` value.
         for dataIndexer in data:
             x = x + 1
 
+            # for string in dataIndexer:
+            #     print(string)
+
+            # print(str(dataIndexer)+"\n")
             if("Xelnect's dark style (inspired)(global)" in dataIndexer['name']):
                 y=x
 
-        print(data[y]['name'])
+                for string in dataIndexer:
+                    print(string + "=" + str(dataIndexer[string]))
+                    # for string2 in string:
+                    #     print(string2 + "=" + str(string2))
 
+                # print(data[y]['sections']['code'])
+                # print(str(dataIndexer)+"\n")
+                # if 'code' in dataIndexer:
+                #     print(dataIndexer['code'])
+                # elif 'name' in dataIndexer:
+                #     print(dataIndexer['name'])
+
+        # print(data[y]['code'])
+
+
+    # css = "/* make input elements more awesome */ input:hover{background-color: red;}"
+    # css = format_css(css)
 
     with open('data.json', 'w') as f:
         json.dump(data, f, indent=4)
