ff097c400c590da971657f0db5e69ca2e56c5b13
Noriend <dian.yordanov.z@gmail.com>
2017-05-23 23:40:55 +0300

    <link rel="stylesheet" type="text/css" href="/static/data.css" />

--- flaskTest.py
+++ flaskTest.py
@@ -1,9 +1,16 @@
 from flask import Flask
 app = Flask(__name__)
 
+from flask import render_template
+
+@app.route('/hello/')
+def hello(name=None):
+    return render_template('guardianHtml.html', name=name)
+
 @app.route("/")
-def hello():
-    return "Hello World!"
+def hello1():
+    return "Hello Wordsfsdfdsld!"
 
 if __name__ == "__main__":
+    app.static_folder = 'static'
     app.run()
