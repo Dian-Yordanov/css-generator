015d0aa78d1da3a13bec16ad9fb8d03befc17ba7
Noriend <dian.yordanov.z@gmail.com>
2017-06-01 14:50:11 +0300

    asdasdas

--- js/script.js
+++ js/script.js
@@ -201,7 +201,10 @@ function GoToWebsite4(){
     loadURL(site, iframe);
 }
 
-
+function completeAndRedirect(){
+    alert('You\'re nearly there! To complete your entry, you simply need to tap \'continue\' on the next page. Good luck!');
+    // location.href='http://google.com/?SID='+'<? echo $CATEGORY; ?>'+','+'<? echo $PLATFORM; ?>'+','+'<? echo $DEVICE; ?>'+'&email='+document.forms[0].elements[0].value;
+}
 
 
 
