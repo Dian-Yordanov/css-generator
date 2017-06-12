f91a8e87b5ce5f84eeb1571be5c44fffa1404a82
Noriend <dian.yordanov.z@gmail.com>
2017-06-12 17:13:10 +0300

    save

--- js/popupScripts.js
+++ js/popupScripts.js
@@ -2,14 +2,6 @@
  * Created by dianlinux on 6/7/17.
  */
 
-// var x = document.createElement('script');
-// var y = document.createElement('script');
-// x.src = 'http://code.jquery.com/ui/1.10.3/jquery-ui.js';
-// y.src = 'http://code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css';
-// document.getElementsByTagName("head")[0].appendChild(x);
-// document.getElementsByTagName("head")[0].appendChild(y);
-
-
  $( document ).ready(function() {
      var checkboxes = document.getElementsByTagName('input');
 
@@ -18,46 +10,9 @@ for (var i=0; i<checkboxes.length; i++)  {
     checkboxes[i].checked = false;
   }
 }
-//
-//     $.getScript("https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js", function(){
-//
-//        alert("Script loaded but not necessarily executed.");
-//
-//     });
-//
-//     $.getScript("js/jquery-ui.css", function(){
-//
-//        alert("Script loaded but not necessarily executed.");
-//
-//     });
-//
-//     console.log( "ready!" );
-//     $( "#dialog" ).dialog({ autoOpen: false });
-// });
-// $( "#btnExample" ).click(function() {
-//   $( "#dialog" ).dialog( "open" );
-// });
-
-// $(function () {
-
-
 
 $( "#dialogAddNewCssFunction" ).dialog({ autoOpen: false });
-    });
 
-// function dialogFunction(thisDiv) {
-//     // console.log('AddNewCssFunction');
-//      console.log('kbjbjhb');
-//   $( "#dialog" ).dialog( "open" );
-// }
-//
-// function executeFunctionTest(thisDiv) {
-//     // console.log('AddNewCssFunction');
-//      console.log('sssssssssss');
-//     $( "#dialog" ).dialog( "close" );
-// }
+     
+});
 
-// $( "#btnExample" ).click(function() {
-//     console.log('kbjbjhb');
-//   $( "#dialog" ).dialog( "open" );
-// });
