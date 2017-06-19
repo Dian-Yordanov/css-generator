3a75d8a6cefb83c16301879b428988143a53d51d
Noriend <dian.yordanov.z@gmail.com>
2017-06-20 02:46:31 +0300

    sync

--- main.py
+++ main.py
@@ -28,10 +28,10 @@ from flask import Flask
 
 
 app = Flask(__name__)
-env = Environments(self.app)
 
-env.from_object('myapp.config')
-env = Environments(self.app, var_name='CUSTOM_VAR_NAME', default_env='CUSTOM_ENV')
+
+
+
 
 filenames = []
 
